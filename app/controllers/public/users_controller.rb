f3ac7f82d98e5361de4dd19007be578a7a759ec2
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-07-12 04:07:06 +0000

    [Fix]登録情報画面の修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -16,10 +16,12 @@ class Public::UsersController < ApplicationController
   
   def recipes
     @user = User.find(params[:id])
-    @users = User.all
-    @recipes = Recipe.all
+    #下記、フォロー/フォロワー一覧表示のためのインスタンス変数
+    @users = @user.followings + @user.followers
+    #下記、@userの投稿ページにその人のレシピ投稿のみ表示させるためのインスタンス変数
+    @recipes = Recipe.where(user_id: @user.id)
   end
-
+  
   #退会フラグはupdateに値するが、マイページ更新の際のupdateアクションと被らないように、destroyアクションに退会処理を記載している
   def destroy
     @user = User.find(params[:id])
