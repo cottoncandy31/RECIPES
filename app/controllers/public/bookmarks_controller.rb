e3bf98f60ca08945dbf704691f6d4f6cdbcc0a0f
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-08-14 13:10:54 +0000

    [Fix]ブックマークボタンの非同期通信化

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -4,21 +4,16 @@ class Public::BookmarksController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
     bookmark = @recipe.bookmarks.new(user_id: current_user.id)
-    if bookmark.save
-      redirect_to request.referer
-    else
-      redirect_to request.referer
-    end
+    bookmark.save
+    #非同期通信化
+    render 'public/bookmarked_recipes/create'
   end
- 
+
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
     bookmark = @recipe.bookmarks.find_by(user_id: current_user.id)
-    if bookmark.present?
-      bookmark.destroy
-      redirect_to request.referer
-    else
-      redirect_to request.referer
-    end
+    bookmark.destroy
+    #非同期通信化
+    render 'public/bookmarked_recipes/destroy'
   end
 end
