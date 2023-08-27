92e3a3fb99007c1ca15ecf1ec60b5ccad49a0a6f
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-08-27 13:31:09 +0000

    [Fix]レシピ編集機能の修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,7 +4,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   def after_sign_up_path_for(resource_or_scope)
-    public_user_path(current_user)
+    public_recipes_path
   end
   # GET /resource/sign_up
   # def new
